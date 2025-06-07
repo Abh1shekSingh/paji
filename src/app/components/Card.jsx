bdac06fe96e464645e97f0c032444398e290a84b
Abhishek Singh <abhinav210702@gmail.com>
2025-06-07 18:55:34 +0530

    Font Changed

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -9,16 +9,16 @@ const Card = ({title, details, index}) => {
         <div className='service-inner relative bg-[var(--color-dark)] w-[100%] min-h-screen flex flex-col'>
             <div className='flex gap-2 border-b-1 border-gray-400'>
               <span className='text-[1rem] md:text-[2rem] opacity-50 subheading'>0{index + 1}</span>
-              <h1 className='heading text-[2rem] md:text-[3.5rem] tracking-wide '>{title}</h1>
+              <h1 className='subheading font-bold text-[2rem] md:text-[3.5rem] tracking-wide '>{title}</h1>
             </div>
             <div className='flex justify-between items-center md:px-10 mt-7'>
               <u className='subheading list-none no-underline'>
                 {details.map((item, i) => (
-                  <li key={i} className='subheading text-[1rem] md:text-[1.25rem]'>{item}</li>
+                  <li key={i} className='heading opacity-70 text-[1rem] md:text-[1.25rem]'>{item}</li>
                 ))}
               </u>
               <div className='w-[30%] h-[30%] overflow-hidden'>
-                <Image src={`/projects/${index + 1}.jpg`} alt='service-images' width={400} height={400}/>
+                <Image src={`/projects/${index + 2}.jpg`} alt='service-images' width={400} height={400}/>
               </div>
             </div>
         </div>
--- src/app/pages/Services.jsx
+++ src/app/pages/Services.jsx
@@ -36,8 +36,8 @@ const Services = React.forwardRef((_props, ref) => {
 
     return (
         <section ref={ref} className='min-h-screen p-10 w-[100%] bg-[var(--color-dark)] sm:rotate-5 sm:scale-[0.5] rounded-t-3xl'>
-            <h1 className='text-[3rem] md:text-[6rem] 2xl:text-[7rem] leading-13 md:leading-30 2xl:leading-tight text-[var(--color-light)] uppercase subheading font-bold '>Web Presence, <span className='text-stroke heading font-light tracking-wider italic'>Perfected.</span></h1>
-            <p className='text-white subheading text-[1.25rem] mt-5 md:text-[1.5rem] opacity-80 md:text-right ml-auto md:mr-7 2xl:text-[1.5rem] md:w-1/2 2xl:w-1/2 md:mt-14 font-light'>From concept to launch, I build beautiful, functional websites that drive results, ensuring your online presence is not just seen, but remembered and acted upon. We focus on seamless user experiences and robust performance to elevate your brand</p>
+            <h1 className='text-[3rem] md:text-[6rem] 2xl:text-[7rem] leading-13 md:leading-30 2xl:leading-tight text-[var(--color-light)] uppercase subheading font-bold '>Web Presence, <span className='text-stroke heading font-light tracking-wider'>Perfected.</span></h1>
+            <p className='text-white heading text-[1.25rem] mt-5 md:text-[1.5rem] opacity-80 md:text-right ml-auto md:mr-7 2xl:text-[1.5rem] md:w-1/2 2xl:w-1/2 md:mt-14 font-light'>From concept to launch, I build beautiful, functional websites that drive results, ensuring your online presence is not just seen, but remembered and acted upon. We focus on seamless user experiences and robust performance to elevate your brand</p>
             {cardsData.map((card, index) => (
                 <Card key={index} {...card} index={index}/>
             ))}
