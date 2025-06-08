44195def82b578eabaed7db947727a42eff81d64
Abhishek Singh <abhinav210702@gmail.com>
2025-06-08 11:59:03 +0530

    Crated the About section

--- src/app/pages/About.jsx
+++ src/app/pages/About.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import Copy from '../components/Copy'
+import Image from 'next/image'
 
 const About = () => {
   
   return (
-    <section className='px-14 text-white h-screen flex justify-center items-center bg-[var(--color-dark)]'>
+    <section className='px-8 py-14 w-[100%] text-white min-h-screen  bg-[var(--color-dark)]'>
       <Copy>
-        <h1 className='text-white font-semibold text-5xl subheading opacity-70 tracking-wider leading-14'>Howdy! I'm Abhishek, and <span className='bg-[#8f00ff] rounded-lg'>I build</span> compelling websites that help clients and brands shine online. When I'm not crafting digital experiences, you'll find me immersed in the beauty of nature, capturing its essence through my <span className='bg-[#ffea00] rounded-lg text-dark'>lens</span>. This portfolio is where my passion for intuitive web design meets my love for the natural world. Let's create something remarkable together.</h1>
+        <h1 className='heading text-[3rem] mb-4 2xl:mb-20 2xl:text-[7rem] 2xl:w-1/2 2xl:leading-27'>Developer & Photographer</h1>
       </Copy>
+      <div className='grid grid-cols-3 gap-7  2xl:gap-14'>
+        <div className='overflow-hidden w-full col-span-3 2xl:col-span-1 rounded-lg grayscale'>
+          <Image src={`/developer/abhishek-4.webp`} alt='developer-image-standing with railing' width={300} height={300} />
+        </div>
+        <div className='col-span-3 2xl:col-span-2 '>
+        <Copy>
+          <h1 className='text-white font-semibold text-[1.5rem] 2xl:text-[3.25rem] subheading opacity-80 tracking-wide leading-8 2xl:leading-17 mb-10'>Howdy! I'm Abhishek, and I build compelling websites that help brands shine online.</h1>
+          <p className='subheading font-light text-[1.15rem] 2xl:text-[1.9rem] 2xl:w-3/4 opacity-70'>When I'm not crafting digital experiences, you'll find me immersed in the beauty of nature, capturing its essence through my <span className='heading italic'>lens</span>. This portfolio is where my passion for intuitive web design meets my love for the natural world.</p>
+        </Copy>
+        </div>
+      </div>
     </section>
   )
 }
--- src/app/pages/CTA.jsx
+++ src/app/pages/CTA.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 
 const CTA = () => {
   return (
-    <section className='w-[100%] h-screen bg-[var(--color-dark)] py-14 px-14'>
-        <div className='w-[100%] h-[100%] bg-[var(--color-light)] my-[14] flex justify-center items-center'>
+    <section className='w-[100%] min-h-screen bg-[var(--color-dark)] py-14 px-14'>
+        <div className='w-[100%] min-h-[100%] bg-[var(--color-light)] my-[14] flex justify-center items-center'>
             <h1 className='heading text-8xl text-center font-bold'>Let's create something remarkable together.</h1>
-            
         </div>
     </section>
   )
